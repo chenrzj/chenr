0d027a6aaf639239c5a1c4794e5f29c9e243a925
92935 <92935@LAPTOP-IHOGL1AS.mshome.net>
2021-02-07 18:38:59 +0800

    chenr

---
+++ src/main/java/com/how2java/j2se/App.java
@@ -0,0 +1,15 @@
+package com.how2java.j2se;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("初始化驱动成功");
+    	System.out.println( "Hello World!" );
+    }
+}
